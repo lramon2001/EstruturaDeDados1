1c1446555b2bd8bc37cdf4c1b9a0bd909c7a51b5
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-05-31 19:07:36 -0300

    Fila completamente implementada

--- src/filas/Fila.java
+++ src/filas/Fila.java
@@ -9,12 +9,17 @@ public class Fila<T> {
         this.listaLigada= new ListaLigada<T>();
     }
     public void enfileirar(T elemento){
-
+        listaLigada.inserir(elemento);
     }
     public T desenfileirar(){
-
+        if(estaVazia()){
+            return null;
+        }
+      T resultado = this.listaLigada.recuperar(0);
+        this.listaLigada.remover(0);
+        return resultado;
     }
     public boolean estaVazia(){
-
+     return this.listaLigada.estaVazia();
     }
 }
