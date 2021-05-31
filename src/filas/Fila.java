2b732fc0e1c97cdd942a842c35c280acfa46f33e
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-05-31 19:01:39 -0300

    Adicionando assinaturas dos métodos da fila

--- src/filas/Fila.java
+++ src/filas/Fila.java
@@ -1,4 +1,20 @@
 package filas;
 
-public class Fila {
+import listasLigadas.ListaLigada;
+
+public class Fila<T> {
+    private ListaLigada<T> listaLigada ;
+
+    public Fila() {
+        this.listaLigada= new ListaLigada<T>();
+    }
+    public void enfileirar(T elemento){
+
+    }
+    public T desenfileirar(){
+
+    }
+    public boolean estaVazia(){
+
+    }
 }
