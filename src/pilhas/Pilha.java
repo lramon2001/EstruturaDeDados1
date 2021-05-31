44ef68092b9fc97410277e3f46fbf52a16b7791c
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-05-31 16:52:38 -0300

    iniciando implementação da pilha

--- src/pilhas/Pilha.java
+++ src/pilhas/Pilha.java
@@ -1,12 +1,4 @@
 package pilhas;
 
-import listasLigadas.ListaDuplamenteLigada;
-
-public class Pilha<T> {
-    private ListaDuplamenteLigada<T> lista;
-
-    public Pilha() {
-        this.lista=new ListaDuplamenteLigada<T>();
-    }
-
+public class Pilha {
 }
